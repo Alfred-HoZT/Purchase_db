61eb921fa94de1dc50219a9c5994c3ab8fa596f5
Alfred-HoZT <105224385+Alfred-HoZT@users.noreply.github.com>
2022-07-15 17:36:10 +0800

    Add files via upload

--- user_interface_draft_3.py
+++ user_interface_draft_3.py
@@ -5,15 +5,15 @@ import sqlite3
 from datetime import date
 from tkinter import filedialog
 from table_creation import *
-# from data_package import *
 
 """
 - README - 
-1. Only execute this file. On execution, app will prompt if you want to reset the database to the original data. Click accordingly.
-2. The top row allows switching between 3 different views, Main, Receive and Search.
-3. Main Frame - User can add new product details into database. Click "Submit" to finalise details.
-4. Receive Frame - does not perform any function currently.
-5. Search Frame - top row with search function does not work. Otherwise, can click "Go" to show all rows in database.
+1. On first try, execute **table_creation** once. You will not need to execute it again in the future.
+2. To start the app, execute file **user_interface_draft_3**. The app will prompt if you want to reset the database to the original data. Click accordingly.
+3. The top row allows switching between 3 different views, Main, Receive and Search.
+4. Main Frame - User can add new product details into database. Click "Submit" to finalise details.
+5. Receive Frame - does not perform any function currently.
+6. Search Frame - top row with search function does not work. Otherwise, can click "Go" to show all rows in database.
 -> Double clicking on each row will fill all entry boxes below accordingly. 
 -> User can change details and click on "Submit" to confirm changes.
 -> Click on "Go" again to view changes made. 
